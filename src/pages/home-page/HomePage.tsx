7471e1e0c369dbcc1af65eabc569e42dbbe0b895
khoanguyn1411 <khoaah1411@gmail.com>
2022-06-26 17:07:01 +0700

    Edit title

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
 
-import { TopBanner } from "./top-banner/TopBanner";
-import { Introduction } from "./introduction/Introduction";
+import { OnAirTVs } from "./on-air-tvs/OnAirTVs";
 import { Popular } from "./popular/Popular";
+import { TopBanner } from "./top-banner/TopBanner";
+import { TopRate } from "./top-rate/TopRated";
 import { Trending } from "./trending/Trending";
 import { UpcommingMovies } from "./upcomming-movies/UpcommingMovies";
-import { TopRate } from "./top-rate/TopRated";
-import { OnAirTVs } from "./on-air-tvs/OnAirTVs";
 
 export const HomePage: React.FC = () => {
   useEffect(() => {
-    document.title = "Home | Universe Cinema";
+    document.title = "Universe Cinema";
   }, []);
   return (
     <div className="w-full mb-[3rem] ">
