867e136f9651a5dad701815df1e582d633cd1090
khoanguyn1411 <khoaah1411@gmail.com>
2022-07-10 19:07:01 +0700

    Remove param

--- src/hooks/useCallAPI.tsx
+++ src/hooks/useCallAPI.tsx
@@ -8,7 +8,7 @@ export const useCallAPI = (api: string, params: any, dependency?: any) => {
       const fetchAPI = async () => {
         try {
           setIsLoading(true);
-          const result = await appAxios.get(api, params && params);
+          const result = await appAxios.get(api, params);
           // console.log(funcs.getAPI(api, ""));
           // const res = await fetch(funcs.getAPI(api, ""));
           // const result = await res.json();
